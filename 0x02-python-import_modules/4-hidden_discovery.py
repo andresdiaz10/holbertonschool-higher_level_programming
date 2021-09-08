a219ba14963c06505069aab0ee324a8c058e8a1a
andresdiaz10 <70444970+andresdiaz10@users.noreply.github.com>
2021-09-08 16:26:48 -0500

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     lists = dir(hidden_4)
-    for u_list in lists:
-        if u_list[0:2] != "__":
-            print(u_list)
+    for name in lists:
+        if name[:2] != "__":
+            print(name)
