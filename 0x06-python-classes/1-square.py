5fb6dfc6ee49a5d4ecaa2750238cadaeb40aed0a
andresdiaz10 <andresdiaz5010@icloud.com>
2021-09-28 18:00:47 -0500

    Add documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-class Square:
+"""Define a Square Class"""
 
+
+class Square:
+    """A Square"""
     def __init__(self, size):
+        """ A new Square.
+
+        Args:
+            size (int): size of the square
+        """
         self.__size = size
