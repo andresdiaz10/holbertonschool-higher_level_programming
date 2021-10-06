f8fe4348601c8caff6eba7c2e2d805e351b05d5c
andresdiaz10 <andresdiaz5010@icloud.com>
2021-10-06 18:53:46 -0500

    TDD project

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#Andres Zapata
+"""A function that adds 2 integers"""
+
+def add_integer(a, b=98):
+    """Return the add of a and b.
+
+    Raises:
+        TypeError: When either of a or b is a non integer or float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
