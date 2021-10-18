53e78abcc66ce972aa81a648ddac5078a553a831
andresdiaz10 <andresdiaz50101@gmail.com>
2021-10-18 14:52:04 -0500

    Task 15 - 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """base.py: Define a base model for geometric objects"""
+import json
 
 
 class Base:
@@ -21,3 +22,21 @@ class Base:
             self.id = Base.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """return a json"""
+        if list_dictionaries == [] or list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                tmp = [index.to_dictionary() for index in list_objs]
+                file.write(Base.to_json_string(tmp))
