9b8685fdfd304f6c49845065cb867673446b2d50
andresdiaz10 <andresdiaz50101@gmail.com>
2021-10-18 11:07:31 -0500

    0-task and set up

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""base.py: Define a base model for geometric objects"""
+
+
+class Base:
+    """Base model for all geometrics classes
+
+    base of all other classes
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Init a new base object
+
+        Arguments:
+            id (int): id of the new base object
+        """
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
