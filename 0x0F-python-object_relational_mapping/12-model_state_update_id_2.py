4d42f2e52981c5c0a1ee342b1d922ebead70ef6b
andres diaz <andres.diaz@foonkiemonkey.co.uk>
2021-12-19 15:46:28 +0000

    Task 11 - 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that changes the name of a State
+object from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from sqlalchemy.orm import session
+
+from sqlalchemy.orm.session import Session
+from model_state import State, Base
+from sqlalchemy import create_engine, engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(State).filter_by(id=2).first().name = "New Mexico"
+    session.commit()
