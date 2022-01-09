541f46c6ddd5ba605dd0de57ee5477dfb2a17733
andresdiaz10 <andresdiaz5010@icloud.com>
2022-01-09 15:34:18 -0500

    task - 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 '''Http status code'''
+from sys import argv
+import urllib.request
+import urllib.error
+
+
+if __name__ == "__main__":
+    client = argv[1]
+    request = urllib.request.Request(client)
+    try:
+        with urllib.request.urlopen(request) as response:
+            print(response.read().decode("ascii"))
+    except urllib.error.HTTPError as statuscode:
+        print("Error code: {}".format(statuscode.code))
