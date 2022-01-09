a57f7495eba07d6a0d181bab8cf75e3fa2f68636
andres diaz <andres.diaz@foonkiemonkey.co.uk>
2022-01-09 12:23:19 -0500

    prueba

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 """script that fetches https://intranet.hbtn.io/status"""
 import urllib.request
-
-
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body res:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
