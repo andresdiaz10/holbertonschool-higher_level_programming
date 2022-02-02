0945e5415325ff3562e6eeba617d6c8d860643f1
andresdiaz10 <andresdiaz5010@icloud.com>
2022-02-02 11:04:13 -0500

    fix semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const file = process.argv[2];
-fs.readFile(file, 'utf8', function (err, data) {
-    console.log(err || data);
+fs.readFile(file, 'utf-8', function (err, data) {
+  console.log(err || data);
 });
